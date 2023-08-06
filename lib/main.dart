1ffbc3de1ee0bdae26444713807ed89c7626b1f8
Muaz <muaz.self@gmail.com>
2023-08-06 15:43:58 +0600

    Update: - Initial Code

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CraftyBay());
+}
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: null,
+      ),
+    );
+  }
+}
