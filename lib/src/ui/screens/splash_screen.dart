94709eb912473b7ef1b8700b0cdb6a46fa164b3a
Muaz <muaz.self@gmail.com>
2023-08-08 12:07:09 +0600

    Add: - Splash Screen add

---
+++ lib/src/ui/screens/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '/src/ui/utils/app_colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Image.asset(
+                'assets/icons/logo.png',
+                width: 100,
+              ),
+            ),
+          ),
+          const Column(
+            children: <Widget>[
+              CircularProgressIndicator(
+                color: secondaryColor,
+                backgroundColor: primaryColor,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 24),
+                child: Text(
+                  'Version 1.0',
+                  style: TextStyle(
+                    color: greyColor,
+                    fontSize: 12,
+                    letterSpacing: 0.6,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
