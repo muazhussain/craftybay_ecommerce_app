e0855319f697f5bb7f9332a206e9d05618b8cf7c
Muaz <muaz.self@gmail.com>
2023-08-09 00:48:20 +0600

    Add: - App different size spacers add.

---
+++ lib/src/ui/constants/app_sizes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+SizedBox spacerH4 = const SizedBox(height: 4);
+SizedBox spacerH8 = const SizedBox(height: 8);
+SizedBox spacerH16 = const SizedBox(height: 16);
+SizedBox spacerH24 = const SizedBox(height: 24);
+SizedBox spacerH32 = const SizedBox(height: 32);
+SizedBox spacerH64 = const SizedBox(height: 64);
+
+SizedBox spacerW4 = const SizedBox(width: 4);
+SizedBox spacerW8 = const SizedBox(width: 8);
+SizedBox spacerW16 = const SizedBox(width: 16);
+SizedBox spacerW24 = const SizedBox(width: 24);
+SizedBox spacerW32 = const SizedBox(width: 32);
+SizedBox spacerW64 = const SizedBox(width: 64);
