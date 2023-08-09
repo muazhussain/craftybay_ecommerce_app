c55de1e00c4b55585296990468d64b65e99ff5bd
Muaz <muaz.self@gmail.com>
2023-08-09 12:57:30 +0600

    Add: - App's common colors add.

---
+++ lib/src/ui/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFF22AEAF);
+const Color secondaryColor = Color(0xFFFC9900);
+const Color greyColor = Color(0xFF757575);
+const Color blackColor = Color(0xFF000000);
