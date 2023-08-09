097bd809d1344ad74da38ce8bfd36623ff174ae5
Muaz <muaz.self@gmail.com>
2023-08-09 12:54:35 +0600

    Update: - Splash screen move to dedicated directory.

--- lib/src/ui/screens/splash/splash_screen.dart
+++ lib/src/ui/screens/splash/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../constants/app_colors.dart';
+import '../../constants/app_colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
